a890ed9e18c8913e1402fa1f9b300820aa85408c
Moses <mov7963@gmail.com>
2023-01-12 14:10:24 -0800

    Changed timeout to promises/async

--- src/context/LinkedListContext.tsx
+++ src/context/LinkedListContext.tsx
@@ -7,6 +7,7 @@ import {
   UpdateNodeDisplay,
 } from "../types/LinkedListTypes";
 import { AppContext, AppContextTypes } from "./AppContextProvider";
+import { resolve } from "path";
 
 export const LinkedListContext = React.createContext<
   LinkedListContextType | undefined
@@ -47,13 +48,16 @@ const LinkedListContextProvider = ({ children }: ReactChildren) => {
   const { toggleAnimation } = React.useContext(AppContext) as AppContextTypes;
 
   /************* Utility Functions **************/
-  const stepDelay = 500;
-  const step = (stateChange: UpdateNodeDisplay, delay: number) => {
-    setTimeout(() => {
-      setDisplay(stateChange);
-    }, delay);
-    return (delay += stepDelay);
+  const stepDelay = 250;
+  const step = (stateChange: UpdateNodeDisplay, delay: number = stepDelay) => {
+    return new Promise<void>((resolve) => {
+      setTimeout(() => {
+        setDisplay(stateChange);
+        resolve();
+      }, delay);
+    });
   };
+
   const clearColorIndicators = () => {
     setDisplay((old) =>
       old.map((node) => {
@@ -65,11 +69,11 @@ const LinkedListContextProvider = ({ children }: ReactChildren) => {
   /************* End Utility Functions **************/
 
   /************* Adding Nodes **************/
-  const addNode = (addToTail: boolean, num: number) => {
+  const addNode = async (addToTail: boolean, num: number) => {
     if (displayList.length === 7) return;
     toggleAnimation(); //says an animation is in progress
     clearColorIndicators();
-    let timeout = 0;
+
     //If there is no head/tail
     if (displayList.length === 0) {
       const newNode = node(num);
@@ -80,52 +84,53 @@ const LinkedListContextProvider = ({ children }: ReactChildren) => {
     }
 
     //Get Tail/Head
-    timeout = step((old) => {
+    await step((old) => {
       const newList = [...old];
 
       const HorT = getHeadOrTail(addToTail, newList);
       // reset new/selector indicators on prepends
       if (newList[HorT]) newList[HorT].selected = true;
       return newList;
-    }, timeout);
+    }, 0);
 
     //Add New Node
-    timeout = step((old) => {
+    await step((old) => {
       const newList = [...old];
       const newNode = node(num);
 
       newNode.new = true;
       newList[addToTail ? "push" : "unshift"](newNode);
       return newList;
-    }, timeout);
+    });
 
     //Add Next Pointer to new Node
-    timeout = step((old) => {
+    await step((old) => {
       const newList = [...old];
       newList[addToTail ? newList.length - 2 : 0].next =
         newList[getHeadOrTail(addToTail, newList)];
       return newList;
-    }, timeout);
+    });
 
     //Removes  Tail Pointer
-    timeout = step((old) => {
+    await step((old) => {
       const newList = [...old];
       newList[getPreHeadOrTail(addToTail, newList)][
         addToTail ? "isTail" : "isHead"
       ] = false;
       return newList;
-    }, timeout);
+    });
 
     // Updates Tail Pointer and resets selected
-    timeout = step((old) => {
+    await step((old) => {
       const newList = [...old];
       newList[getPreHeadOrTail(addToTail, newList)].selected = false;
       newList[getHeadOrTail(addToTail, newList)][
         addToTail ? "isTail" : "isHead"
       ] = true;
-      toggleAnimation();
       return newList;
-    }, timeout);
+    });
+
+    toggleAnimation();
   };
 
   const append = (num: number) => {
@@ -138,42 +143,46 @@ const LinkedListContextProvider = ({ children }: ReactChildren) => {
   /************* End Adding Nodes **************/
 
   /************* Removing Nodes **************/
-  const removeNode = (removeFromTail: boolean) => {
+  const removeNode = async (removeFromTail: boolean) => {
     if (displayList.length === 0) return;
     toggleAnimation();
     clearColorIndicators();
-    let timeout = 0;
+    console.log("started");
+
+    let oneNodeLeft = false;
 
     // Get Head Or Tail
-    timeout = step((old) => {
+    await step((old) => {
+      oneNodeLeft = old.length === 1;
+      console.log(oneNodeLeft, 'insode step');
+      
       const newList = [...old];
       const HorT = getHeadOrTail(removeFromTail, newList);
       // reset new/selector indicators on prepends
       if (newList[HorT]) newList[HorT].selected = true;
       return newList;
-    }, timeout);
+    }, 0);
+    console.log(oneNodeLeft, 'outside step');
 
     // if there is only one node left remove last node
-    if (displayList.length === 1) {
-      console.log("one left");
-
-      step(() => {
-        toggleAnimation();
-        return [];
-      }, timeout);
+    if (oneNodeLeft) {
+      console.log('early end');
+      
+      await step([]);
+      toggleAnimation();
       return;
     }
 
     // Get New Head/ Tail / Remov
-    timeout = step((old) => {
+    await step((old) => {
       const newList = [...old];
       const newHeadorTail = getPreHeadOrTail(removeFromTail, newList);
       newList[newHeadorTail].selected = true;
       return newList;
-    }, timeout);
+    });
 
     // Indicate Remove Old Head/Tail and switch pointer
-    timeout = step((old) => {
+    await step((old) => {
       const newList = [...old];
       const newHeadorTail = getPreHeadOrTail(removeFromTail, newList);
       const headOrTail = getHeadOrTail(removeFromTail, newList);
@@ -194,20 +203,18 @@ const LinkedListContextProvider = ({ children }: ReactChildren) => {
 
       // Remove appropriate next pointers
       newList[removeFromTail ? newList.length - 2 : 0].next = null;
-
-      toggleAnimation();
       return newList;
-    }, timeout);
+    });
 
     //Delete Node
-    timeout = step((old) => {
+    await step((old) => {
       const newList = [...old];
       newList[removeFromTail ? "pop" : "shift"]();
       return newList;
-    }, timeout);
+    });
+    console.log("done");
 
-    // To chain remove Node functions
-    return;
+    toggleAnimation();
   };
 
   const removeHead = () => {
@@ -218,22 +225,16 @@ const LinkedListContextProvider = ({ children }: ReactChildren) => {
     removeNode(true);
   };
 
-  const clear = (instant?: boolean) => {
+  const clear = async (instant?: boolean) => {
+    toggleAnimation();
     if (instant) {
       setDisplay([]);
     } else {
-      let timeout = 0;
-      for (let i = 0; i < displayList.length; ) {
-        console.log(timeout, "befoer");
-
-        setTimeout(() => {
-          removeNode(false);
-          i++
-          timeout += stepDelay;
-        }, timeout);
-        console.log(timeout, "after update");
+      for (let i = 0; i < displayList.length; i++) {
+        await removeNode(false);
       }
     }
+    toggleAnimation();
   };
   /************* End Removing Nodes **************/
 
